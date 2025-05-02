d41ec545b875cd0d65c4b4fff0d27141979665e4
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-01 22:45:15 -0300

    add while

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
-horas = int(input('digite'))
+horas = int(input('digite as horas por dia'))
+
+while True: 
+  mes = int(input('digite quantos dias vc trabalha no mes'))
+  if mes <= 31:
+   break
+  else:
+   print('Digite um numero válido sua Anta!')
+
+salario = int(input('digite seu salario agora clt'))
+
+soma = (horas * mes)
+soma1 = (soma / salario)
+
+
+
+
+if mes > 30: 
+  print('digite um numero valido')
+  
+
+
 
 
 
